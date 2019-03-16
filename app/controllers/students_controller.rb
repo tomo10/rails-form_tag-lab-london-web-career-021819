2cfc0560ebb1d799252dfc960dc585b752706282
Thomas Edwards <thce28@gmail.com>
2019-03-16 18:09:12 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+  #need a new and create route for the students resource
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: [:student][:last_name])
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+s1 = Student.create(first_name: "Thomas", last_name: "Edwards")
+s2 = Student.create(first_name: "Odel", last_name: "Beckham Jr")
+s3 = Student.create(first_name: "Kobe", last_name: "Bryant")
